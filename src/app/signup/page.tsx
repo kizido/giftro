52c037027fb65b22a553b3dd22891e5a032d0e46
kizido <kieranking3555@gmail.com>
2024-02-18 18:50:01 -0800

    feat: create login and signup routes

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='w-full h-screen flex items-center justify-center'>
+        <div className='w-1/3 h-fit border-2 border-black border-dashed p-8'>
+            <h1>Hello</h1>
+        </div>
+    </div>
+  )
+}
