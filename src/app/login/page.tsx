a05824b281b3d4525cced2763c849869464b515d
kizido <kieranking3555@gmail.com>
2024-02-18 19:41:58 -0800

    feat: create and style login page form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="w-full pt-24 flex justify-center gap-8">
+      <div className="w-96 p-2 flex flex-col gap-2">
+        <h1 className="mb-4 text-3xl font-semibold">Log in</h1>
+        <hr className="border border-slate-300"></hr>
+        <h3 className="text-lg">Username</h3>
+        <input type="text" className="w-full h-10 rounded"></input>
+
+        <div className="flex justify-between">
+          <h3 className="text-lg">Password</h3>
+          <Link href="/forgotpassword" className="text-blue-700 underline">
+            Forgot password?
+          </Link>
+        </div>
+        <input type="text" className="mb-4 w-full h-10 rounded"></input>
+
+        <div className="flex items-center">
+          <input type="checkbox" className="mr-2 h-6 w-6 rounded-lg" />
+          <h4 className="inline">Remember my login</h4>
+        </div>
+
+        <div className="flex justify-end">
+          <button className="w-28 h-12 bg-blue-400 font-bold">Log In</button>
+        </div>
+      </div>
+      <div className="w-96 p-2 flex flex-col gap-2">
+        <h1 className="mb-4 text-3xl font-semibold">Sign Up</h1>
+
+        <hr className="border border-slate-300"></hr>
+
+        <h3 className="font-semibold">New to Gift App? Sign up now to start finding perfect gifts for your loved ones!</h3>
+
+        <div className="flex justify-end">
+          <button className="w-28 h-12 bg-blue-400 font-bold">Join Now</button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,17 +3,17 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="fixed w-full h-16 bg-gray-800 flex items-center justify-between p-8">
+    <nav className="fixed w-full h-20 bg-gray-800 flex items-center justify-between p-8">
       <div>
-        <Link href="/" className="text-white">
+        <Link href="/" className="text-white text-4xl">
           Gift App
         </Link>
       </div>
       <div className="flex justify-center gap-8">
-        <Link href="/login" className="text-white">
+        <Link href="/login" className="text-white text-2xl">
           Login
         </Link>
-        <Link href="/signup" className="text-white">
+        <Link href="/signup" className="text-white text-2xl">
           Sign Up
         </Link>
       </div>
