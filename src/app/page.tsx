f7b207b0a4082ff77ebf89168979f86c580d7cae
kizido <kieranking3555@gmail.com>
2024-02-18 18:49:50 -0800

    feat: create and style basic landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <div>
+      <div className="w-full px-16 py-16 flex items-center justify-center flex-col bg-slate-400 gap-8">
+        <h1 className="text-6xl">Hello, welcome to Gift App</h1>
+        <h1 className="text-3xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum velit non condimentum sollicitudin. Morbi id ante ipsum. Fusce in iaculis justo, sit amet aliquet lorem. Donec at pretium dolor. Sed convallis, arcu vitae eleifend tristique, ex purus gravida augue, at varius erat augue et est. Aliquam pretium neque mi, malesuada pulvinar tellus elementum eu. Duis fringilla laoreet odio lobortis aliquam. Suspendisse in diam felis. Nunc porta vestibulum faucibus. Mauris ut lorem vitae quam lobortis pretium et id ante. In ipsum nulla, pellentesque lobortis ipsum sed, porttitor sagittis nunc. Donec et felis eget nulla imperdiet aliquam sed id ligula. Donec vitae leo ut eros convallis dictum eget ut mauris. Suspendisse ex neque, suscipit quis lacus faucibus, volutpat convallis ante. Nulla ex tellus, hendrerit at ligula eget, faucibus vulputate risus. Fusce semper molestie sem ut venenatis.</h1>
+      </div>
+      <div className="w-full px-16 py-16 flex items-center justify-center bg-slate-700 gap-12">
+        <h1 className="w-1/3 text-8xl">Duis fringilla laoreet odio lobortis aliquam</h1>
+        <h1 className="w-2/3 text-4xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum velit non condimentum sollicitudin. Morbi id ante ipsum. Fusce in iaculis justo, sit amet aliquet lorem. Donec at pretium dolor. Sed convallis, arcu vitae eleifend tristique, ex purus gravida augue, at varius erat augue et est. Aliquam pretium neque mi, malesuada pulvinar tellus elementum eu. Duis fringilla laoreet odio lobortis aliquam. Suspendisse in diam felis. Nunc porta vestibulum faucibus. Mauris ut lorem vitae quam lobortis pretium et id ante. In ipsum nulla, pellentesque lobortis ipsum sed, porttitor sagittis nunc. Donec et felis eget nulla imperdiet aliquam sed id ligula. Donec vitae leo ut eros convallis dictum eget ut mauris. Suspendisse ex neque, suscipit quis lacus faucibus, volutpat convallis ante. Nulla ex tellus, hendrerit at ligula eget, faucibus vulputate risus. Fusce semper molestie sem ut venenatis.</h1>
       </div>
     </main>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,17 +3,17 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="w-full h-16 bg-gray-800 flex items-center justify-between p-8">
+    <nav className="fixed w-full h-16 bg-gray-800 flex items-center justify-between p-8">
       <div>
         <Link href="/" className="text-white">
           Gift App
         </Link>
       </div>
       <div className="flex justify-center gap-8">
-        <Link href="/" className="text-white">
+        <Link href="/login" className="text-white">
           Login
         </Link>
-        <Link href="/" className="text-white">
+        <Link href="/signup" className="text-white">
           Sign Up
         </Link>
       </div>
