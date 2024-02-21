94ba51f4e5c53b513b935549b46acc8656a6eab4
kizido <kieranking3555@gmail.com>
2024-02-20 16:06:15 -0800

    style: make login page perfectly responsive

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,16 +3,16 @@ import React from "react";
 
 export default function Login() {
   return (
-    <div className="w-full pt-24 flex flex-col md:flex-row items-center md:items-start justify-center gap-8">
-      <div className="w-96 p-2 flex flex-col gap-2">
+    <div className="w-full min-w-48 p-4 pt-6 md:pt-24 flex flex-col md:flex-row items-center md:items-start justify-center gap-2 md:gap-8">
+      <div className="w-full max-w-[28rem] p-2 flex flex-col gap-2">
         <h1 className="mb-4 text-3xl font-semibold">Log in</h1>
         <hr className="border border-slate-300"></hr>
-        <h3 className="text-lg">Username</h3>
+        <h3 className="text-base">Username</h3>
         <input type="text" className="p-4 h-10 rounded"></input>
 
         <div className="flex justify-between">
-          <h3 className="text-lg">Password</h3>
-          <Link href="/forgotpassword" className="text-blue-700 underline">
+          <h3 className="text-base">Password</h3>
+          <Link href="/forgotpassword" className="text-sm text-blue-700 underline">
             Forgot password?
           </Link>
         </div>
@@ -27,7 +27,7 @@ export default function Login() {
           <button className="w-28 h-12 bg-blue-400 font-bold">Log In</button>
         </div>
       </div>
-      <div className="w-96 p-2 flex flex-col   gap-2">
+      <div className="w-full max-w-[28rem] p-2 flex flex-col gap-2">
         <h1 className="mb-4 text-3xl font-semibold">Sign Up</h1>
 
         <hr className="border border-slate-300"></hr>
