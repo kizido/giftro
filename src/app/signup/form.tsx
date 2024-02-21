f4529bb67ce4ac4b62f1889493b22e0faeb98eb7
kizido <kieranking3555@gmail.com>
2024-02-20 18:13:29 -0800

    feat: create api route for signup

---
+++ src/app/signup/form.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import Link from "next/link";
+import { FormEvent } from "react";
+
+export default function Form() {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const response = await fetch("/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify({
+        email: formData.get("email"),
+        password: formData.get("password"),
+      }),
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="w-full flex justify-center">
+      <div className="w-70 flex flex-col gap-2">
+        <h1 className="mt-8 font-bold text-3xl">Create an account</h1>
+        <h3 className="text-xl text-gray-200">It's free!</h3>
+
+        <hr className="mb-4 border border-slate-300"></hr>
+
+        <div className="mb-8">
+          <h4 className="inline text-sm text-gray-200">
+            Do you already have an account?{" "}
+          </h4>
+          <Link href="/login" className="inline text-sm text-teal-300">
+            Log in
+          </Link>
+        </div>
+
+        <h3 className="text-lg">E-Mail</h3>
+        <input type="email" name='email' className="h-10 p-4 rounded"></input>
+
+        {/* <h3 className="text-lg">Username</h3>
+        <input className="h-10 rounded"></input> */}
+
+        <div className="flex justify-between items-center">
+          <h3 className="text-lg">Password</h3>
+          <h4 className="cursor-pointer hover:underline text-sm text-teal-300">
+            Show password
+          </h4>
+        </div>
+        <input type="password" name='password' className="mb-4 p-4 h-10 rounded"></input>
+
+        <div className="flex justify-end">
+          <button type="submit" className="w-28 h-12 bg-blue-400 font-bold">
+            Join Now
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,42 +1,7 @@
-import Link from "next/link";
-import React from "react";
+import Form from "./form";
 
-export default function page() {
+export default async function page() {
   return (
-    <div className="w-full flex justify-center">
-      <div className="w-70 flex flex-col gap-2">
-        <h1 className="mt-8 font-bold text-3xl">Create an account</h1>
-        <h3 className="text-xl text-gray-200">It's free!</h3>
-
-        <hr className="mb-4 border border-slate-300"></hr>
-
-        <div className="mb-8">
-          <h4 className="inline text-sm text-gray-200">
-            Do you already have an account?{" "}
-          </h4>
-          <Link href="/login" className="inline text-sm text-teal-300">
-            Log in
-          </Link>
-        </div>
-
-        <h3 className="text-lg">E-Mail</h3>
-        <input className="h-10 rounded"></input>
-
-        <h3 className="text-lg">Username</h3>
-        <input className="h-10 rounded"></input>
-
-        <div className="flex justify-between items-center">
-          <h3 className="text-lg">Password</h3>
-          <h4 className="cursor-pointer hover:underline text-sm text-teal-300">
-            Show password
-          </h4>
-        </div>
-        <input className="mb-4 h-10 rounded"></input>
-
-        <div className="flex justify-end">
-          <button className="w-28 h-12 bg-blue-400 font-bold">Join Now</button>
-        </div>
-      </div>
-    </div>
+    <Form />
   );
 }
