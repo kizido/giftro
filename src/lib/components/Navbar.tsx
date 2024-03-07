1a779c4e359f052457dc300751d6c5ea9fec993a
kizido <kieranking3555@gmail.com>
2024-03-07 14:18:05 -0800

    feat: make home button redirect to dashboard when logged in

--- src/lib/components/Navbar.tsx
+++ src/lib/components/Navbar.tsx
@@ -8,18 +8,23 @@ export default async function Navbar() {
   return (
     <nav className="fixed w-full h-20 bg-gray-800 flex items-center justify-between p-8">
       <div>
-        <Link href="/" className="text-white md:text-4xl text-2xl">
+        <Link
+          href={!!session ? "/dashboard" : "/"}
+          className="text-white md:text-4xl text-2xl"
+        >
           Gift App
         </Link>
       </div>
-      {!session && <div className="flex justify-center md:gap-8 gap-4">
-        <Link href="/login" className="text-white md:text-2xl text-xl">
-          Login
-        </Link>
-        <Link href="/signup" className="text-white md:text-2xl text-xl">
-          Sign Up
-        </Link>
-      </div>}
+      {!session && (
+        <div className="flex justify-center md:gap-8 gap-4">
+          <Link href="/login" className="text-white md:text-2xl text-xl">
+            Login
+          </Link>
+          <Link href="/signup" className="text-white md:text-2xl text-xl">
+            Sign Up
+          </Link>
+        </div>
+      )}
       {!!session && <Logout />}
     </nav>
   );
