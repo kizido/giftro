ec190b429c2712c368bbea769e77bd3d3fb6a64b
Kieran King <kieranking3555@gmail.com>
2024-03-21 12:05:13 -0700

    feat: layout skeleton for search area + catergories of gift finder page

--- src/app/my-list/page.tsx
+++ src/app/my-list/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import React from "react";
 
@@ -6,9 +7,19 @@ export default function MyLists() {
     <div className="container max-w-[72rem] h-screen gap-12 mt-8">
       <div className="h-full flex justify-between">
         {/* Gift Searching Area */}
-        <div className="w-[45rem] border border-black border-dashed">
+        <div className="w-[45rem]">
           <h1 className="text-3xl font-semibold">Gift Finder</h1>
-          <Input />
+          <div className="py-4 flex flex-col gap-2">
+            <Input className="h-14 text-md" placeholder="Search for gifts here..." />
+            <div className="flex flex-wrap gap-2 justify-start items-center">
+              <Button iconUrl="https://static-cdn.drawnames.com//Content/Assets/chevron-gray.svg" iconRotation={90} variant="secondary" >Category</Button>
+              <Button variant="secondary" iconUrl="https://static-cdn.drawnames.com//Content/Assets/chevron-gray.svg" iconRotation={90} >Shop</Button>
+              <Button variant="secondary" iconUrl="https://static-cdn.drawnames.com//Content/Assets/chevron-gray.svg" iconRotation={90}>$0 - $20</Button>
+              <Button variant="secondary" iconUrl="https://static-cdn.drawnames.com//Content/Assets/chevron-gray.svg" iconRotation={90}>18 -25 Years</Button>
+              <Button variant="secondary" iconUrl="https://static-cdn.drawnames.com//Content/Assets/chevron-gray.svg" iconRotation={90}>Men</Button>
+              <h3 className="ml-1 text-md text-semibold text-blue-500 cursor-pointer hover:underline">Clear all filters</h3>
+            </div>
+          </div>
         </div>
 
         {/* Wish List Area */}
