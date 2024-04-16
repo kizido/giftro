6a1cb296c838378d3246fc7f562acf888b6adfcf
Kieran King <kieranking3555@gmail.com>
2024-04-16 12:12:15 -0700

    style: style activity boxes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -32,24 +32,34 @@ export default function Page() {
   return (
     <div className="w-full h-full flex justify-between items-center px-20">
       {/* Upcoming Events */}
-      <div className="pt-4 w-60 h-4/5 border border-black">
+      <div className="pt-4 px-4 w-72 h-4/5 shadow-[0_0_8px_0px_rgba(0,0,0,0.5)] rounded-lg flex flex-col gap-4">
         <h2 className="text-center font-semibold">Upcoming Events</h2>
+        <p>May 12 - Mother's Day</p>
+        <p>June 14 - Kyle's Birthday</p>
       </div>
 
       {/* Trending/Popular Items */}
       <div className="px-24 py-24 w-full self-start flex flex-wrap justify-center gap-x-12 gap-y-12">
+        <div className="w-44 h-28 border border-black">
+          <h2 className="text-center py-10 font-semibold">Trending</h2>
+        </div>
+        <div className="w-44 h-28 border border-black">
+          <h2 className="text-center py-10 font-semibold">Most Popular</h2>
+        </div>
+        <div className="w-44 h-28 border border-black">
+          <h2 className="text-center py-10 font-semibold">Seasonal</h2>
+        </div>
+        <div className="w-44 h-28 border border-black">
+          <h2 className="text-center py-10 font-semibold">Mother's Day</h2>
+        </div>
+        {/* <div className="w-44 h-28 border border-black"></div>
         <div className="w-44 h-28 border border-black"></div>
         <div className="w-44 h-28 border border-black"></div>
-        <div className="w-44 h-28 border border-black"></div>
-        <div className="w-44 h-28 border border-black"></div>
-        <div className="w-44 h-28 border border-black"></div>
-        <div className="w-44 h-28 border border-black"></div>
-        <div className="w-44 h-28 border border-black"></div>
-        <div className="w-44 h-28 border border-black"></div>
+        <div className="w-44 h-28 border border-black"></div> */}
       </div>
 
       {/* Recent Friend Activity */}
-      <div className="pt-4 w-60 h-4/5 border border-black">
+      <div className="pt-4 w-72 h-4/5 shadow-[0_0_8px_0px_rgba(0,0,0,0.5)] rounded-lg">
         <h2 className="text-center font-semibold">Recent Activity</h2>
       </div>
       {isFirstTimeUser && modalOpen && (
