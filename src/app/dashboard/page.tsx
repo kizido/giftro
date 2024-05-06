dac9e8f9f95e60503516754113ef1ebf63dac4e0
Kieran King <kieranking3555@gmail.com>
2024-05-06 13:00:45 -0700

    feat: begin upcoming events display list on dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,18 +5,21 @@ import { Input } from "@/components/ui/input";
 import { useDebounceValue } from "@/hooks/useDebounceValue";
 import { QueryResultRow } from "@vercel/postgres";
 import EventDisplayModal from "@/components/ui/eventDisplayModal";
+import { TCreateEvent } from "@/lib/types";
 
 export default function Page() {
   const [isFirstTimeUser, setIsFirstTimeUser] = useState(false);
   const [modalOpen, setModalOpen] = useState(true);
 
-  const [filterState, setFilterState] = useState('trending');
+  const [filterState, setFilterState] = useState("trending");
   const [eventDisplayModal, setEventDisplayModal] = useState(false);
+
+  const [upcomingEvents, setUpcomingEvents] = useState<TCreateEvent[]>([]);
 
   useEffect(() => {
     checkIsFirstTimeUser();
+    loadUpcomingEvents();
   }, []);
-  
 
   const checkIsFirstTimeUser = async () => {
     try {
@@ -33,33 +36,75 @@ export default function Page() {
     }
   };
 
+  const loadUpcomingEvents = async () => {
+    try {
+      const response = await fetch("/api/events", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const responseData = await response.json();
+      console.log(responseData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="w-full h-full flex justify-between items-center px-72">
       {/* Upcoming Events */}
       <div className="pt-4 px-4 w-80 h-4/5 shadow-[0_0_8px_0px_rgba(0,0,0,0.5)] rounded-lg flex flex-col gap-4 bg-gray-50">
-
         {/* Event Display Modal */}
-        {eventDisplayModal && <EventDisplayModal onClose={() => setEventDisplayModal(false)}/>}
+        {eventDisplayModal && (
+          <EventDisplayModal onClose={() => setEventDisplayModal(false)} />
+        )}
 
         <h2 className="text-center font-semibold">Upcoming Events</h2>
-        <p className="cursor-pointer hover:text-gray-400" onClick={() => setEventDisplayModal(true)}>{`May 12 - Mother's Day`}</p>
-        <p className="cursor-pointer hover:text-gray-400" onClick={() => setEventDisplayModal(true)}>{`June 14 - Kyle's Birthday`}</p>
+        <p
+          className="cursor-pointer hover:text-gray-400"
+          onClick={() => setEventDisplayModal(true)}
+        >{`May 12 - Mother's Day`}</p>
+        <p
+          className="cursor-pointer hover:text-gray-400"
+          onClick={() => setEventDisplayModal(true)}
+        >{`June 14 - Kyle's Birthday`}</p>
       </div>
 
       {/* Trending/Popular Items */}
       <div className="px-24 pt-24 w-full self-start">
         {/* Item Filters */}
         <div className="w-full self-start flex flex-wrap justify-center gap-x-12 gap-y-12 content-box">
-          <div className={`flex justify-center items-center w-36 h-28 border-2 border-black bg-red-200 cursor-pointer ${filterState === 'trending' ? 'border-4 border-yellow-200' : ''}`} onClick={() => setFilterState('trending')}>
+          <div
+            className={`flex justify-center items-center w-36 h-28 border-2 border-black bg-red-200 cursor-pointer ${
+              filterState === "trending" ? "border-4 border-yellow-200" : ""
+            }`}
+            onClick={() => setFilterState("trending")}
+          >
             <h2 className="font-semibold">Trending</h2>
           </div>
-          <div className={`flex justify-center items-center w-36 h-28 border-2 border-black bg-blue-200 cursor-pointer ${filterState === 'popular' ? 'border-4 border-yellow-200' : ''}`} onClick={() => setFilterState('popular')}>
+          <div
+            className={`flex justify-center items-center w-36 h-28 border-2 border-black bg-blue-200 cursor-pointer ${
+              filterState === "popular" ? "border-4 border-yellow-200" : ""
+            }`}
+            onClick={() => setFilterState("popular")}
+          >
             <h2 className="text-center py-10 font-semibold">Most Popular</h2>
           </div>
-          <div className={`flex justify-center items-center w-36 h-28 border-2 border-black bg-orange-200 cursor-pointer ${filterState === 'seasonal' ? 'border-4 border-yellow-200' : ''}`} onClick={() => setFilterState('seasonal')}>
+          <div
+            className={`flex justify-center items-center w-36 h-28 border-2 border-black bg-orange-200 cursor-pointer ${
+              filterState === "seasonal" ? "border-4 border-yellow-200" : ""
+            }`}
+            onClick={() => setFilterState("seasonal")}
+          >
             <h2 className="text-center py-10 font-semibold">Seasonal</h2>
           </div>
-          <div className={`flex justify-center items-center w-36 h-28 border-2 border-black bg-green-200 cursor-pointer ${filterState === 'holiday' ? 'border-4 border-yellow-200' : ''}`} onClick={() => setFilterState('holiday')}>
+          <div
+            className={`flex justify-center items-center w-36 h-28 border-2 border-black bg-green-200 cursor-pointer ${
+              filterState === "holiday" ? "border-4 border-yellow-200" : ""
+            }`}
+            onClick={() => setFilterState("holiday")}
+          >
             <h2 className="text-center py-10 font-semibold">Mothers Day</h2>
           </div>
         </div>
