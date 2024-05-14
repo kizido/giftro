45008187fce567faddf87e67477fe7a107c4bfe5
Kieran King <kieranking3555@gmail.com>
2024-05-14 11:32:03 -0700

    feat: wip remove item from wish list button

--- src/components/ui/wishList.tsx
+++ src/components/ui/wishList.tsx
@@ -20,9 +20,9 @@ const WishList = () => {
           },
         });
         const response = await request.json();
-        if(!response.error) {
-            const likedItems: ListItem[] = response;
-            setListItems(likedItems);
+        if (!response.error) {
+          const likedItems: ListItem[] = response;
+          setListItems(likedItems);
         }
       } catch (error) {
         console.log(error);
@@ -31,15 +31,33 @@ const WishList = () => {
     getListItems();
   }, []);
 
+  async function removeFromWishlist(asin: string) {
+    try {
+      await fetch("/api/products/likedProducts", {
+        method: "POST",
+        body: JSON.stringify(asin),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="w-full h-full">
-        <div className="w-full bg-blue-400 p-4 mb-4">
-            <h1 className="text-center font-bold text-2xl">My Wish List</h1>
-        </div>
-        <div className="px-4">
-      {listItems.map((item) => (
-        <WishListItem key={item.asin} wishListItem={item} />
-      ))}
+      <div className="w-full bg-blue-400 p-4 mb-4">
+        <h1 className="text-center font-bold text-2xl">My Wish List</h1>
+      </div>
+      <div className="px-4">
+        {listItems.map((item) => (
+          <WishListItem
+            key={item.asin}
+            wishListItem={item}
+            onClose={() => removeFromWishlist(item.asin)}
+          />
+        ))}
       </div>
       {/* <button className="block p-2 bg-gray-200">Add Item</button> */}
     </div>
--- src/components/ui/wishListItem.tsx
+++ src/components/ui/wishListItem.tsx
@@ -1,13 +1,25 @@
 import { ListItem } from "./wishList";
 
 type WishListItemProps = {
-    wishListItem: ListItem;
-}
-const WishListItem = ({wishListItem}: WishListItemProps) => {
+  wishListItem: ListItem;
+  onClose: () => void;
+};
+const WishListItem = ({ wishListItem, onClose }: WishListItemProps) => {
 
-    return (<div className="w-full mb-4 p-2 border border-black flex items-center">
+  return (
+    <div className="relative w-full mb-4 p-2 border border-black flex items-center">
+      <img
+        src="https://static-cdn.drawnames.com/Content/Assets/icon-close.svg"
+        width={20}
+        className="absolute top-2 right-2 cursor-pointer"
+        onClick={onClose}
+        alt="Wish List Item"
+      />
+      <div className="w-11/12">
         <h2>{wishListItem.name ?? wishListItem.asin}</h2>
-    </div>);
-}
+      </div>
+    </div>
+  );
+};
 
 export default WishListItem;
