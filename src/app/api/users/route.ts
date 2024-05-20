05552579a85a881622dfd2163a5ce94bd72d8407
Kieran King <kieranking3555@gmail.com>
2024-05-20 10:54:55 -0700

    feat: filter current friends out of 'add friend' search

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -5,22 +5,38 @@ import { authOptions } from "../auth/[...nextauth]/options";
 
 export async function GET(request: NextRequest) {
   const session = await getServerSession(authOptions);
-  const searchParams = request.nextUrl.searchParams
-  const query = searchParams.get('query');
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
 
   try {
     if (!session) {
       return NextResponse.json({ error: "No session established." });
     }
-    if(query === null) {
-        return NextResponse.json({ error: "No query provided." });
+    if (query === null) {
+      return NextResponse.json({ error: "No query provided." });
     }
+    // const retrievedUsersResult = await sql`
+    //   SELECT * FROM users
+    //   WHERE username ILIKE ${query + "%"}
+    //   AND id != ${session.id}
+    //   LIMIT 10
+    // `;
     const retrievedUsersResult = await sql`
-      SELECT * FROM users
-      WHERE username ILIKE ${query + "%"}
-      AND id != ${session.id}
-      LIMIT 10
-    `;
+    SELECT * 
+    FROM users
+    WHERE username ILIKE ${query + "%"}
+    AND id != ${session.id}
+    AND id NOT IN (
+      SELECT 
+        CASE 
+          WHEN user_id = ${session.id} THEN friend_user_id
+          ELSE user_id
+        END
+      FROM user_friends
+      WHERE status = 'accepted'
+      AND (user_id = ${session.id} OR friend_user_id = ${session.id})
+    )
+    LIMIT 10`;
     const retrievedUsers = retrievedUsersResult.rows;
     return NextResponse.json({
       retrievedUsers,
