202c9132856c37a398840183da651dce506e7713
kizido <kieranking3555@gmail.com>
2024-07-02 20:38:51 -0700

    feat: add hamburger nav icon to navbar on mobile view

--- src/lib/components/Navbar.tsx
+++ src/lib/components/Navbar.tsx
@@ -5,56 +5,64 @@ import CreateEventButton from "@/components/ui/createEventButton";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
 import React from "react";
+import { IconMenu2 } from "@tabler/icons-react";
 
 export default async function Navbar() {
   const session = await getServerSession(authOptions);
+
   return (
     <nav className="fixed w-full h-20 bg-background border-b z-30">
-      <div className="container max-w-[70rem] flex items-center justify-center gap-12 py-4 px-1">
+      <div className="h-full px-16 xl:px-32 flex items-center justify-between py-4">
         <Link
           href={!!session ? "/dashboard" : "/"}
-          className="text-foreground md:text-2xl text-xl flex-1"
+          className="text-foreground text-xl"
         >
           Gift App
         </Link>
-        {!session && (
-          <div className="flex justify-center md:gap-8 gap-4">
-            <Link
-              href="/login"
-              className="bg-secondary text-secondary-foreground p-2 md:text-lg text-md rounded-md"
-            >
-              Login
-            </Link>
-            <Link
-              href="/signup"
-              className="bg-primary text-primary-foreground p-2 md:text-lg text-md rounded-md"
-            >
-              Sign Up
-            </Link>
-          </div>
-        )}
         {!!session && (
           <>
-            <Link href={"/dashboard"} className="text-semibold text-md">
-              Home
-            </Link>
-            <Link href={"/friends"} className="text-semibold text-md">
-              Friends
-            </Link>
-            <Link href={"/my-list"} className="text-semibold text-md">
-              My List
-            </Link>
-            <Link
-              href={"/upcoming-birthdays"}
-              className="text-semibold text-md"
-            >
-              Upcoming Birthdays
-            </Link>
-            <Link href={"/profile"} className="text-semibold text-md">
-              Profile
-            </Link>
-            <CreateEventButton />
-            <Logout />
+            <div className="hidden lg:flex gap-8 xl:gap-12 items-center">
+              <Link href={"/dashboard"} className="text-semibold text-md">
+                Home
+              </Link>
+              <Link href={"/friends"} className="text-semibold text-md">
+                Friends
+              </Link>
+              <Link href={"/my-list"} className="text-semibold text-md">
+                My List
+              </Link>
+              <Link
+                href={"/upcoming-birthdays"}
+                className="text-semibold text-md"
+              >
+                Upcoming Birthdays
+              </Link>
+              <Link href={"/profile"} className="text-semibold text-md">
+                Profile
+              </Link>
+              <CreateEventButton />
+            </div>
+
+            <div className="hidden lg:block">
+              <Logout />
+            </div>
+            <div className="lg:hidden flex items-center gap-4">
+              <CreateEventButton />
+              <IconMenu2
+                size={32}
+                className="cursor-pointer"
+              />
+            </div>
+          </>
+        )}
+        {!session && (
+          <>
+            <Button variant="outline">
+              <Link href="/login">Login</Link>
+            </Button>
+            <Button variant="default">
+              <Link href="/signup">Sign Up</Link>
+            </Button>
           </>
         )}
       </div>
